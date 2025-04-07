c3d6f5dddc835b5f2731f7cad4aa38977a1abe76
jonathandw743 <jonathandw414@gmail.com>
2025-04-07 18:35:19 +0100

    option

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use iter_variants_derive::impl_iter_variants_tuple;
 pub use iter_variants_derive::IterVariants;
+use iter_variants_derive::impl_iter_variants_tuple;
 
 pub trait IterVariants {
     type T;
@@ -11,6 +11,17 @@ impl IterVariants for bool {
     fn iter_variants<F: Fn(Self::T)>(f: F) {
         f(false);
         f(true);
+    }
+}
+
+impl<U: IterVariants> IterVariants for Option<U>
+where
+    <U as IterVariants>::T: IterVariants,
+{
+    type T = Option<<U as IterVariants>::T>;
+    fn iter_variants<F: Fn(Self::T)>(f: F) {
+        f(None);
+        U::iter_variants(|v| f(Some(v)));
     }
 }
 
